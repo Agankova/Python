1b15e9b08634d122d681d3c76d581257761a1e26
Karpenko <maria.k.visage@gmail.com>
2023-04-04 21:13:37 +0300

    внесено решение

--- S3/task16.py
+++ S3/task16.py
@@ -7,15 +7,13 @@
 # -> 1
 
 
-N = abs(int(input('Введите количество элементов списка А: ')))
-A_entered = input("Введите через пробел элементы списка: ").split()
-A_num = list(map(int, A_entered))
-if len(A_num) != N:
-    print('Введенные элементы не соответствуют заявленному количеству!')
-else:
-    X = int(input('Введите число X, которое необходимо найти в списке: '))
-    count = 0
-    for i in range(N):
-        if A_num[i] == X:
-            count += 1
-    print(f'Число {X} встречается в списке A {count} раз') 
+N = int(input('Введите размер массиве: '))
+A = input("Введите элементы массива: ")
+A_n = list(map(int, A))
+X = int(input('Введите число X: '))
+count = 0
+for i in range(N):
+    if A_n[i] == X:
+     count += 1
+print(f'Число {X} встречается {count} раз') 
+
